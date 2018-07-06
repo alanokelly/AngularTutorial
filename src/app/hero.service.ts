ce0e5f4464ae5f229d2f437b66146baac82337db
Alan O'Kelly <alan@dealervision.com>
2018-07-06 13:14:32 -0700

    trying out a commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './classes/hero';
+import { HEROES } from './constants/mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    //TODO: Send the message after fetching heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`)
+    return of(HEROES.find(hero => hero.id === id))
+  }
+}
